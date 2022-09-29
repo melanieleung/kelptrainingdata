70df54e906a6855904322d14ace688209de16e41
Melanie Leung <49533829+melanieleung@users.noreply.github.com>
2022-09-29 13:09:28 -0700

    Update trainingData_formatting.py
    Setting directory through arcpy instead of through the os package. Splits the file name so that .tif extension is not included when Python reads through each file

--- trainingData_formatting.py
+++ trainingData_formatting.py
@@ -2,7 +2,7 @@ import os
 import arcpy
 from arcpy import management
 
-os.chdir(r'C:\Users\mleung\Desktop\santaBarbara\classifications') #change to file path for your classifications
+os.chdir(r'C:\Users\mleung\Desktop\sanDiego\classifications') #change to file path for your classifications
 os.listdir()
 
 # Extract by Mask for sites likes British Columbia
@@ -19,29 +19,48 @@ ExtractByMask(in_raster,
               {extraction_area}, 
               {analysis_extent})
 
+# sets current directory through arcpy instead of os package
+arcpy.env.workspace = "C:/Users/mleung/Desktop/sanDiego/classifications"
+
+walk = arcpy.da.Walk(datatype="RasterDataset")
+
+for dir_path, dir_names, file_names in walk:
+    for filename in file_names:
+        print(os.path.join(dir_path, filename))
+
+fileList = arcpy.ListFiles("*.tif")
+print(fileList)
+
+# lists files and stores them 
+fileList = arcpy.ListFiles("*.tif")
+
+for raster in arcpy.ListFiles("*.tif"):
+    # get the file name without extension (replaces the %Name% variable from ModelBuidler)
+    fileName = os.path.splitext(raster)[0]
+    # define the output file
+    rastFile = fileName + "_final.tif"
+    # run the copy raster tool
+    arcpy.management.CopyRaster(raster,
+                                rastFile,
+                                '', 
+                                '',
+                                3,
+                                'NONE',
+                                'NONE',
+                                '8_BIT_UNSIGNED',
+                                'NONE',
+                                'NONE', # color map to RGB
+                                'TIFF',
+                                'NONE',
+                                'CURRENT_SLICE',
+                                'NO_TRANSPOSE')
+
 # for loop to automate Copy Raster tool
-for raster in os.listdir(r'C:\Users\mleung\Desktop\santaBarbara\classifications'): #change file path
+for raster in os.listdir(r'C:\Users\mleung\Desktop\santaBarbara\classifications_noAux'): #change file path
     if raster.endswith('.tif'):
         outRaster = raster.replace('.tif', '_final.tif')
         arcpy.management.CopyRaster(raster,
                                    outRaster,
-                                    '', 
-                                    '',
-                                    3,
-                                    'NONE',
-                                    'NONE',
-                                    '8_BIT_UNSIGNED',
-                                    'NONE',
-                                    'NONE', # color map to RGB
-                                    'TIFF',
-                                    'NONE',
-                                    'CURRENT_SLICE',
-                                    'NO_TRANSPOSE')
-
-arcpy.ValidateFieldName('20200827_175133_89_222c_3B_AnalyticMS_SR_8b_harmonized_clip.tif')
-
-arcpy.management.CopyRaster('20200827_175133_89_222c_3B_AnalyticMS_SR_8b_harmonized_clip.tif',
-                                   '20200827_175133_89_222c_3B_AnalyticMS_SR_8b_harmonized_clip_final.tif',
                                     '', 
                                     '',
                                     3,
